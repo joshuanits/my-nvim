e55c988962a5dd68640f1878eface832afbee4c7
Joshua Nitschke <joshua.nitschke@homestart.com.au>
2025-01-02 09:18:15 +1030

    bind telescope symbols

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>fd", require'telescope.builtin'.lsp_document_symbols, { desc = "Telescope find document symbols" })
+map("n", "<leader>fw", require'telescope.builtin'.lsp_dynamic_workspace_symbols, { desc = "Telescope find workspace symbols" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
